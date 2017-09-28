514b15831dd31f8b3bdf84cdd8ab6e3ea0355dda
SUNVIEW <32350723+hakunacode@users.noreply.github.com>
2017-09-28 20:20:06 +0800

    initial

---
+++ main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SpinArt
+//
+//  Created by Rim Rami on 10/13/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+	NSAutoreleasePool *pool = [NSAutoreleasePool new];
+	int retVal = UIApplicationMain(argc, argv, nil, @"SpinArtAppDelegate");
+	[pool release];
+	return retVal;
+}
